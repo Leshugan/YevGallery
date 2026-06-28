a5f0f9b551a2148b458a363da9ed5dcc3def44fd
Yevhenii <Leshugan@gmail.com>
2026-06-28 14:12:06 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "leshugan.yg",
+  appName: "YevGallery",
+  webDir: "dist",
+  android: {
+    allowMixedContent: false,
+  },
+};
+
+export default config;
