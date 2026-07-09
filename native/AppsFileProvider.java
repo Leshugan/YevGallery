7ba6109cf77561311b882af9914a6f4f45867706
Yevhenii <Leshugan@gmail.com>
2026-07-09 12:34:42 +0300

    Delete native directory

--- native/AppsFileProvider.java
+++
@@ -1,8 +0,0 @@
-package leshugan.yg;
-
-import androidx.core.content.FileProvider;
-
-// Отдельный подкласс, чтобы НЕ конфликтовать с FileProvider, который регистрирует Capacitor
-// (иначе authority leshugan.yg.appsfp «слипается» с провайдером Capacitor и редактор падает с SecurityException).
-public class AppsFileProvider extends FileProvider {
-}
